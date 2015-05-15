85b03aa3550c15184215c27ed2b91091735318c8
Aaron Delani <aaron@aarondelani.com>
2015-05-15 13:31:03 -0700

    Fixing error on print page

--- inventory/view_control.php
+++ inventory/view_control.php
@@ -10,6 +10,8 @@
 
 	$query = "SELECT `paradox`.`inv_item`.`itemid` AS `itemid`, inv_item.item_keyword AS item_keyword, inv_item.style AS Style, inv_item.color AS Color, inv_item.styleinfo AS Fabric, inv_item.item_brand AS Brand, inv_item.item_pattern, inv_item.item_fit AS Fit, inv_item.item_origin AS Origin, inv_item.image AS image, (`oh`.`size_os_oh` - `so`.`size_os_commit`) AS `OS`, (`oh`.`size_2xs_oh` - `so`.`size_2xs_commit`) AS `2XS`, (`oh`.`size_xs_oh` - `so`.`size_xs_commit`) AS `XS`, (`oh`.`size_s_oh` - `so`.`size_s_commit`) AS `S`, (`oh`.`size_m_oh` - `so`.`size_m_commit`) AS `M`, (`oh`.`size_l_oh` - `so`.`size_l_commit`) AS `L`, (`oh`.`size_xl_oh` - `so`.`size_xl_commit`) AS `XL`, (`oh`.`size_2xl_oh` - `so`.`size_2xl_commit`) AS `2XL`, (`oh`.`size_3xl_oh` - `so`.`size_3xl_commit`) AS `3XL`, (`oh`.`size_4xl_oh` - `so`.`size_4xl_commit`) AS `4XL`, ( ( ( ( ( ( ( ( ( (`oh`.`size_2xs_oh` - `so`.`size_2xs_commit`) + (`oh`.`size_2xs_oh` - `so`.`size_2xs_commit`) ) + (`oh`.`size_xs_oh` - `so`.`size_xs_commit`) ) + (`oh`.`size_s_oh` - `so`.`size_s_commit`) ) + (`oh`.`size_m_oh` - `so`.`size_m_commit`) ) + (`oh`.`size_l_oh` - `so`.`size_l_commit`) ) + (`oh`.`size_xl_oh` - `so`.`size_xl_commit`) ) + (`oh`.`size_2xl_oh` - `so`.`size_2xl_commit`) ) + (`oh`.`size_3xl_oh` - `so`.`size_3xl_commit`) ) + (`oh`.`size_4xl_oh` - `so`.`size_4xl_commit`) ) AS `size_avail_total` FROM ( (`paradox`.`inv_item` JOIN `paradox`.`view_item_oh_counts` `oh` ON ( (`oh`.`itemid` = `paradox`.`inv_item`.`itemid`) ) ) JOIN `paradox`.`view_item_so_counts_v2` `so` ON ( (`so`.`itemid` = `paradox`.`inv_item`.`itemid`) ) ) WHERE ";
 
+	$default = "inv_item.item_remove = 'active' GROUP BY `paradox`.`inv_item`.`itemid`;";
+
 	$lji_rack_page = FALSE;
 	$all_inventory = TRUE;
 	$current_page = "Viewing All Inventory";
@@ -18,10 +20,6 @@
 		if ($_GET["t"] == 1){
 			$testing = TRUE;
 		}
-	}
-
-	if (!isset($_GET["rep"]) && !isset($_POST["submit"]) || $_GET["rep"] == "all") {
-		$query .= "inv_item.item_remove = 'active' GROUP BY `paradox`.`inv_item`.`itemid`;";
 	}
 
 	if (isset($_GET["rep"])) {
@@ -42,6 +40,12 @@
 
 			$query .= "inv_item.item_keyword LIKE '%" . $filterQuery . "%' AND inv_item.item_remove = 'active' GROUP BY `paradox`.`inv_item`.`itemid`;";
 		}
+
+		if ($_GET["rep"] == "all") {
+			$query .= $default;
+		}
+	} else {
+		$query .= $default;
 	}
 
 	if (isset($_POST["submit"])) {
