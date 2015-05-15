f300b922af2963d784733971274206783f6fcb92
Aaron Delani <aaron@aarondelani.com>
2015-05-15 11:41:44 -0700

    Here are the last fixes for the login page

--- admin/vars.php
+++ admin/vars.php
@@ -26,11 +26,11 @@ if ($errs) {
 session_start();
 // print_r($_SERVER);
 
-if ($_SERVER['HTTP_REFERER'] != "" && isset($_SESSION['url'])) {
-	$_SESSION['url'] = $_SERVER['HTTP_REFERER'];
-} else {
-	$_SESSION['url'] = $host;
-}
+// if ($_SERVER['HTTP_REFERER'] != "" && isset($_SESSION['url'])) {
+// 	$_SESSION['url'] = $_SERVER['HTTP_REFERER'];
+// } else {
+// 	$_SESSION['url'] = $host;
+// }
 
 if (!isset($_SESSION['login_tries'])) {
 	$_SESSION['login_tries'] = 0;
